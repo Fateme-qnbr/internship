d6f9d2ab654d7ef45d086755e7960141deb43860
Fateme_qnbr <fatemeqanbari010@gmail.com>
2024-11-06 16:06:58 +0330

    calculate

---
+++ src/Calculator/Main.java
@@ -0,0 +1,50 @@
+package Calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//      Scanner definition
+        Scanner input1 = new Scanner(System.in);
+        Scanner input2 = new Scanner(System.in);
+        Scanner input3 = new Scanner(System.in);
+
+//      take a num from user
+        float num1 = input1.nextFloat();
+        System.out.println(" enter the  first number: "+num1);
+        float num2 = input2.nextFloat();
+        System.out.println(" enter the  second number: "+num2);
+
+//      definition user operation choice
+
+        System.out.println("which operation you want to calculate?");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("Enter your choice: ");
+        int choice = input3.nextInt();
+
+//
+
+        switch (choice){
+            case 1:
+                System.out.println("Addition choose: "+(num1+num2));
+                break;
+            case 2:
+                System.out.println("Subtraction choose: "+(num1-num2));
+                break;
+            case 3:
+                System.out.println("Multiplication choose: "+(num1*num2));
+                break;
+            case 4:
+                System.out.println("Division choose: "+(num1/num2));
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+
+        }
+
+    }
+}
